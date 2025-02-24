013e789c75808b30bfd300a15741dcf877ce16e3
aldottexe <aldottexe@gmail.com>
2025-02-24 14:10:59 -0500

    initial commit

---
+++ src/lib/partViewer.svelte.ts
@@ -0,0 +1,115 @@
+import * as THREE from 'three';
+
+import { BloomEffect, NoiseEffect, KawaseBlurPass, EffectComposer, EffectPass, RenderPass } from "postprocessing";
+
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { UltraHDRLoader } from 'three/addons/loaders/UltraHDRLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+let camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer;
+let composer: EffectComposer;
+let piston: any;
+
+export function init(node: HTMLCanvasElement) {
+
+	// RENDERER
+	renderer = new THREE.WebGLRenderer({ antialias: false, canvas: node, alpha: false});
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.toneMapping = THREE.ACESFilmicToneMapping;
+	renderer.toneMappingExposure = 5
+
+	// CAMERA
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
+	camera.position.z = 2;
+
+	const orbit = new OrbitControls(camera, node)
+
+	// SCENE
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color("#fee")
+
+	// HDRI
+	// convert to gainmap using
+	// https://gainmap-creator.monogrid.com/
+	const loader = new UltraHDRLoader();
+	loader.setDataType(THREE.FloatType);
+	loader.load('3.jpg', t => {
+		console.log(t);
+		t.mapping = THREE.EquirectangularReflectionMapping;
+		t.needsUpdate = true;
+
+		// scene.background = t;
+		scene.environment = t;
+	});
+
+	// LIGHTS
+	// const light = new THREE.PointLight(0xc0b3ed, 20);
+	// const light2 = new THREE.PointLight(0x7587a1, 20);
+	// const light3 = new THREE.AmbientLight(0xffffff, 1);
+	//
+	// light.position.set(4, 0, 2);
+	// light2.position.set(-2, 0, -2);
+	//
+	// scene.add(light);
+	// scene.add(light2);
+	// scene.add(light3);
+	//
+	// scene.add(new THREE.PointLightHelper(light));
+	// scene.add(new THREE.PointLightHelper(light2));
+
+	// OBJECT
+	const objLoader = new OBJLoader();
+	objLoader.load('piston.obj', (root) => {
+		piston = root
+		piston.scale.set(.4, .4, .4);
+		scene.add(piston);
+		piston.rotation.z += 1;
+
+		const mat = new THREE.MeshStandardMaterial({ metalness: .8, roughness: .6 })
+		piston.children.forEach((c: THREE.Mesh) => c.material = mat);
+	});
+
+	renderer.setAnimationLoop(animate);
+
+	// EFFECTS
+	composer = new EffectComposer(renderer);
+
+	const renderPass = new RenderPass(scene, camera);
+	composer.addPass(renderPass);
+
+	const blur = new KawaseBlurPass();
+	blur.scale = .5;
+	composer.addPass(blur);
+
+	const bloom = new BloomEffect();
+
+	const noise = new NoiseEffect()
+	noise.blendMode.opacity = new THREE.Uniform(.2);
+
+	const bloomPass = new EffectPass(camera, bloom, noise);
+	composer.addPass(bloomPass);
+
+	window.addEventListener('resize', onWindowResize);
+}
+
+// RESIZE
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+// ANIMATE
+function animate() {
+
+	if (piston) {
+		piston.rotation.y += 0.005;
+	}
+
+	composer.render();
+
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { init } from "$lib/partViewer.svelte";
+   import {type Snippet} from 'svelte';
+   let { children } :  {children: Snippet} = $props();
+</script>
+
+  <canvas use:init></canvas>
+
+<h1>
+  hello!
+</h1>
+
+{@render children()}
+
+<style>
+  :global {
+  html {
+    background-color: #fee;
+  }
+  }
+
+  canvas {
+    position: fixed;
+    z-index: -1;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
